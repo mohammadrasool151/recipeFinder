696d83d5fa681fddcc42bc697a95403e3293b2dd
Rasool <mohammad.11901876@lpu.in>
2021-07-13 19:03:18 +0530

    Final code

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import styles from "../styles/Footer.module.css";
+
+const Footer = () => (
+	<div className={styles.footer}>
+		<div>Made with 💝 with React ⚛️ by </div>
+		<div>
+			{" "}
+			&copy;{" "}
+			<a href="https://github.com/mohammadrasool151" target="_blank" rel="noreferrer">
+				Mohammad Raimport styles from "../styles/Footer.module.css";
+
+const Footer = () => (
+	<div className={styles.footer}>
+		<div>Made with 💝 with React ⚛️ by </div>
+		<div>
+			{" "}
+			&copy;{" "}
+			<a href="https://github.com/mohammadrasool151" target="_blank" rel="noreferrer">
+				Mohammad Rasool
+			</a>
+		</div>
+	</div>
+);
+
+export default Footer;
